61e448247a58d8a54614e0a65dca66a16b14d21f
riekohojo <lieco1031@gmail.com>
2024-05-07 01:13:48 +0900

    0506

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,11 @@
 /*slick slide*/
+$(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed:1000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+});
